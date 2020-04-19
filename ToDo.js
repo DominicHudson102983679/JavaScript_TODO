a62d5fde000cd0905d932d9f9be6fe396a349d32
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-04-19 15:06:23 +1000

    continued working on js, added comments

--- ToDo.js
+++ ToDo.js
@@ -1,4 +1,4 @@
-
+/*
 const EntryInput = document.getElementById("EntryInput");
 const myUL = document.getElementById("myUL");
 const clearBtn = document.getElementById("clear-tasks");
@@ -10,15 +10,15 @@ function loadEventListeners(){
   EntryInput.addEventListener("submit", addTask)
 }
 
-function newElement(e) {
-  if(taskInput.value === ''){
+function addTask(e) {
+  if(EntryInput.value === ''){
     alert("No Task Entered");
   }
 
   else {
 
   const li = document.createElement("li");
-  li.className = "myUL";
+  li.className = "li";
   li.appendChild(document.createTextNode(EntryInput.value));
 
   const link = document.createElement("a");
@@ -26,7 +26,7 @@ function newElement(e) {
   link.innerHTML = "<i class='fa fa-remove'></i>";
   li.appendChild(link)
 
-  taskInput.appendChild(li);
+  EntryInput.appendChild(li);
 
   taskInput.value = '';}
 
@@ -40,32 +40,117 @@ function newElement(e) {
 
   e.preventDefault();
 }
+*/
 
 
 
+loadEventListeners();
 
-/*
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+
 
 function newElement() {
+    // creates a li element to build the list
     var li = document.createElement("li");
+    // converts text entered into bar into inputValue
     var inputValue = document.getElementById("EntryInput").value;
+    // stores text entered into bar as text
     var t = document.createTextNode(inputValue);
+    //adds the text that was input onto the previously built li element
     li.appendChild(t);
+
+    // 'no text entered' pop up alert
     if (inputValue === '') {
       alert("No Text Entered");
-    } else {
+    } 
+    // if field isnt left blank, calls the ul element and adds the text as a li element
+    else {
       document.getElementById("myUL").appendChild(li);
     }
+ 
+    //clears bar after entry
     document.getElementById("EntryInput").value = "";
-  
+    }
+
+    /*
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
       }
     }
-  }
-  
   */
+  
+ 
+ function clearElement() {
+  document.getElementById("myUL").value = "";
+}
+
+
+
+/* ---------- attempted js going off of a different Udemy js tutorial, couldnt get it working
+
+const EntryInput = document.getElementById("EntryInput");
+const myUL = document.getElementById("myUL");
+const clearBtn = document.getElementById("clear-tasks");
+const taskInput = document.getElementById("taskTitle");
+
+loadEventListeners();
+
+function loadEventListeners(){
+  EntryInput.addEventListener("submit", addTask)
+}
+
+function addTask(e) {
+  if(EntryInput.value === ''){
+    alert("No Task Entered");
+  }
+
+  else {
+
+  const li = document.createElement("li");
+  li.className = "li";
+  li.appendChild(document.createTextNode(EntryInput.value));
+
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  link.innerHTML = "<i class='fa fa-remove'></i>";
+  li.appendChild(link)
+
+  EntryInput.appendChild(li);
+
+  taskInput.value = '';}
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+
+  e.preventDefault();
+}
+*/
+
   
   
